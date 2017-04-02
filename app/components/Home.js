681917819600d59feb4df56dfa9888c4f15d802d
wizzy25 <womayuku@gmail.com>
2017-04-02 23:49:46 +0200

    setup react routes (client-side)

---
+++ app/components/Home.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div className='alert alert-info'>
+        Welcome Home! Williams says hello :-)
+      </div>
+    )
+  }
+}
+
+export default Home
