0379cc3cecaee39116aee19008605bc54ecf2447
wizzy25 <womayuku@gmail.com>
2017-04-16 15:43:48 +0200

    Add footer component

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 'use strict'
 
 import React from 'react'
+import Footer from './Footer'
 
 class App extends React.Component {
   render() {
     return(
       <div>
         {this.props.children}
+        <Footer />
       </div>
     )
   }
