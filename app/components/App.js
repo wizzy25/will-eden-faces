759ae2aaf091bde2337f560c2fe4f824482d5999
wizzy25 <womayuku@gmail.com>
2017-04-16 22:27:12 +0200

    Add Navbar component

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,14 @@
 'use strict'
 
 import React from 'react'
+import Navbar from './Navbar'
 import Footer from './Footer'
 
 class App extends React.Component {
   render() {
     return(
       <div>
+        <Navbar />
         {this.props.children}
         <Footer />
       </div>
